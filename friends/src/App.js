21e621e4f1fc39d241902974ace665e34de882e3
Jordan Hanson <JORDANMERCEDES22@GMAIL.COM>
2021-01-07 02:18:22 -0700

    links and routes in app created

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import { Link, Route, Switch } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+        <li>
+          <Link to="/logout">Logout</Link>
+        </li>
+        <Switch>
+          Login Form
+          <Route path="/login" />
+          Protected Route view Friends after Sign In
+          <Route path="/newfriend" />
+          Protected Friend Form that directs to sign in if no token in storage
+          <Route path="/friends" />
+          Redirect to Home
+          <Route path="/" />
+        </Switch>
+      </ul>
     </div>
+
   );
 }
 
