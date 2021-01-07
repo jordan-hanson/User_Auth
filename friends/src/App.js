9aabfa153f14435d05d9224a9f18e4ad904da53d
Jordan Hanson <JORDANMERCEDES22@GMAIL.COM>
2021-01-07 04:40:09 -0700

    axios post on login gets a token

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,27 +1,20 @@
 import './App.css';
-import { Link, Route, Switch } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
+import { LoginForm } from './components/loginForm'
 
 function App() {
   return (
     <div className="App">
-      <ul>
-        <li>
-          <Link to="/login">Login</Link>
-        </li>
-        <li>
-          <Link to="/logout">Logout</Link>
-        </li>
-        <Switch>
-          Login Form
-          <Route path="/login" />
+      <Switch>
+        Login Form
+          <Route path="/login" component={LoginForm} />
           Protected Route view Friends after Sign In
           <Route path="/newfriend" />
           Protected Friend Form that directs to sign in if no token in storage
           <Route path="/friends" />
           Redirect to Home
-          <Route path="/" />
-        </Switch>
-      </ul>
+          <Route path="/" component={LoginForm} />
+      </Switch>
     </div>
 
   );
---
+++ friends/src/components/loginForm.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react'
+import { axiosWithAuth } from '../utils/axioswithAuth'
+
+export const LoginForm = (props) => {
+
+    const [userValues, setUserValues] = useState({
+        username: '',
+        password: ''
+    })
+
+    const onChange = (e) => {
+        const newUser = {
+            ...userValues,
+            [e.target.name]: e.target.value
+        }
+        setUserValues(newUser)
+    }
+
+    const onSubmit = (e) => {
+        // console.log(props)
+        // console.log(userValues)
+        e.preventDefault();
+        axiosWithAuth()
+            .post('http://localhost:5000/api/login', userValues)
+            .then(res => {
+                console.log(res.data.payload)
+                localStorage.setItem('token', res.data.payload)
+                props.history.push('/protected')
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }
+
+    return (
+        <div>
+            <h1>Friendbook</h1>
+            <h3>Add your friends. Stay connected.</h3>
+            <form onSubmit={onSubmit}>
+                <input value={userValues.username} onChange={onChange} name="username" placeholder="Username"></input>
+                <input value={userValues.password} onChange={onChange} name="password" placeholder="Password"></input>
+                <button>Login</button>
+            </form>
+        </div>
+    )
+}
