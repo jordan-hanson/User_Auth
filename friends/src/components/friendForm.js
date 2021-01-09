2bab41190873fa6bd5d38c9a4c53c577d6420e56
Jordan Hanson <JORDANMERCEDES22@GMAIL.COM>
2021-01-08 21:51:46 -0700

    friend form state made

---
+++ friends/src/components/friendForm.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+export const FriendForm = () => {
+    const [friendValues, setFriendValues] = useState({
+        name: '',
+        age: 0,
+        email: '',
+    })
+}
